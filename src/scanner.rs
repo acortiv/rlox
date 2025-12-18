84b8d757040adb3700088eff4295941b339c0d55
acortiv <tacortiv@proton.me>
2025-12-18 12:08:49 -0700

    feat: string literal parsing

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,4 @@
+use crate::error::error;
 use crate::token::{Literal, Token, TokenType};
 
 // Source needs to be made into U8 as Rust uses UTF-8... indexing strings is unsafe and O(n)
@@ -48,8 +49,90 @@ impl Scanner {
             b'+' => self.add_token(TokenType::Plus),
             b';' => self.add_token(TokenType::Semicolon),
             b'*' => self.add_token(TokenType::Star),
-            _ => (),
+            b'!' => {
+                let t = if self.match_char(b'=') {
+                    TokenType::BangEqual
+                } else {
+                    TokenType::Bang
+                };
+                self.add_token(t)
+            }
+            b'=' => {
+                let t = if self.match_char(b'=') {
+                    TokenType::EqualEqual
+                } else {
+                    TokenType::Equal
+                };
+                self.add_token(t)
+            }
+            b'<' => {
+                let t = if self.match_char(b'=') {
+                    TokenType::LessEqual
+                } else {
+                    TokenType::Less
+                };
+                self.add_token(t)
+            }
+            b'>' => {
+                let t = if self.match_char(b'=') {
+                    TokenType::GreaterEqual
+                } else {
+                    TokenType::Greater
+                };
+                self.add_token(t)
+            }
+            b'/' => {
+                if self.match_char(b'/') {
+                    while self.peek() != b'\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash)
+                }
+            }
+            b' ' | b'\r' | b'\t' => (),
+            b'\n' => self.line += 1,
+            b'"' => self.parse_string(),
+            _ => error(self.line, "Unexpected character."),
         }
+    }
+
+    fn parse_string(&mut self) {
+        while self.peek() != b'"' && !self.is_at_end() {
+            if self.peek() == b'\n' {
+                self.line += 1
+            };
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            error(self.line, "Unterminated string.");
+            return;
+        }
+
+        self.advance();
+        let start = &self.start + 1;
+        let end = &self.current - 1;
+        let text = &self.source[start..end];
+        self.add_token_prime(TokenType::String, Literal::Str(text.to_string()))
+    }
+
+    fn match_char(&mut self, expected: u8) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+        if self.source.as_bytes()[self.current] != expected {
+            return false;
+        }
+        self.current += 1;
+        true
+    }
+
+    fn peek(&self) -> u8 {
+        if self.is_at_end() {
+            return b'\0';
+        }
+        self.source.as_bytes()[self.current]
     }
 
     fn is_at_end(&self) -> bool {
